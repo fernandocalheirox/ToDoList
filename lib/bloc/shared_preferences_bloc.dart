b1d795c3768c5498f63df2f7bb64bffc65efab63
Fernando Calheiros <crd.yoshi@gmail.com>
2021-01-14 11:17:51 -0300

    future bloc

--- lib/bloc/shared_preferences_bloc.dart
+++ lib/bloc/shared_preferences_bloc.dart
@@ -1,10 +1,4 @@
 import 'dart:convert';
-
 import 'package:shared_preferences/shared_preferences.dart';
 
-class SharedBloc {
-  save(var valueSaved) async {
-    var prefs = await SharedPreferences.getInstance();
-    await prefs.setString('data', jsonEncode(valueSaved));
-  }
-}
+class SharedBloc {}
