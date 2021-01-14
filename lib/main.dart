e57456779646938c2672d28942bbbb8b07c6d8ee
Fernando Calheiros <crd.yoshi@gmail.com>
2021-01-14 10:15:29 -0300

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/pages/home_page.dart';
-
-import 'pages/loading_page.dart';
+import 'pages/load_page.dart';
 
 void main() {
   runApp(MyApp());
